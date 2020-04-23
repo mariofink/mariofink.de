26efea49674d17af942408e69be00e32871c0813
Mario Fink <mario.fink@goodyear.com>
2020-04-23 21:23:42 +0200

    readd seo

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -4,23 +4,23 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class Homepage extends React.Component {
+class Impressum extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="Home" />
+        <SEO title="Impressum" />
         <article>
-          <h2>Home</h2>
+          <h2>Impressum</h2>
         </article>
       </Layout>
     )
   }
 }
 
-export default Homepage
+export default Impressum
 
 export const pageQuery = graphql`
   query {
