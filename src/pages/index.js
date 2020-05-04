fc96518365d5c3f7dc6993626f35384977edfa98
Mario Fink <mario.fink@goodyear.com>
2020-05-04 22:15:02 +0200

    copy text from current homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,34 @@ class Homepage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="Home" />
         <article>
-          <h2>Home</h2>
+          <h2>
+            <span role="img" aria-label="Hallo">
+              👋
+            </span>
+            !
+          </h2>
+          <p>
+            Mein Name ist Mario Fink — ich arbeite als Webentwickler &amp; UX
+            Designer in Fulda.
+          </p>
+          <p>
+            <em>Konkret heißt das,</em> ich entwerfe und baue Webseiten die
+          </p>
+          <ul>
+            <li>leicht zugänglich sind</li>
+            <li>sich auf das Wesentliche konzentrieren</li>
+            <li>den Besucher nicht mit unnötigem Klimm-Bimm verwirren</li>
+          </ul>
+          <p>
+            <em>Im Detail heißt das,</em> ich beschäftige mich mit
+          </p>
+          <ul>
+            <li>HTML5, CSS3 &amp; JavaScript</li>
+            <li>Responsive Design</li>
+            <li>Progressive Enhancement</li>
+            <li>Benutzer-zentriertem Design</li>
+            <li>vielem mehr</li>
+          </ul>
           <p>
             <Link to="/posts">Artikel</Link>
           </p>
